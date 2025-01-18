570e233120be0dea124a30852c4e45fbf6f75409
Savva <imperiya66.ru@gmail.com>
2025-01-18 14:46:48 +0300

    Dima.Savva

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import Header from "@/components/header/header";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -22,6 +23,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <Header></Header>
         {children}
       </body>
     </html>
--- src/app/products/papirus/page.jsx
+++ src/app/products/papirus/page.jsx
@@ -1,7 +1,10 @@
 "use client"
-import React, { use } from 'react'
+import React, { use, useEffect, useState } from 'react'
 import styles from './styles.module.css'
 import { MatrixRain } from '@/components/MatrixRain/MatrixRain';
+
+import styles1 from "./Book.module.css";
+import HTMLFlipBook from "react-pageflip";
 
 import image1 from "../../../assets/papir/papir6.jpg";
 import image2 from "../../../assets/papir/papir8.jpg";
@@ -14,21 +17,42 @@ import image8 from "../../../assets/papir/papyrus5.jpg";
 import image9 from "../../../assets/papir/papyrus6.jpg";
 import image10 from "../../../assets/papir/papyrus7.jpg";
 import image11 from "../../../assets/papir/papyrus8.jpg";
-// import image12 from "../../../assets/papir/papyrus9.jpg";
-// import image13 from "../../../assets/papir/papyrus10.jpg";
-// import image14 from "../../../assets/papir/papyrus11.jpg";
-// import image15 from "../../../assets/papir/papyrus12.jpg";
-// import image16 from "../../../assets/papir/papyrus13.jpg";
-// import image17 from "../../../assets/papir/papyrus14.jpg";
-// import image18 from "../../../assets/papir/000010.jpg";
-// import image19 from "../../../assets/papir/000008.jpg";
-// import image20 from "../../../assets/papir/000001.jpg";
-// import image21 from "../../../assets/papir/000011.jpg";
+import image12 from "../../../assets/papir/papyrus9.jpg";
+import image13 from "../../../assets/papir/papyrus10.jpg";
+import image14 from "../../../assets/papir/papyrus11.jpg";
+import image15 from "../../../assets/papir/papyrus11.jpg";
+import image16 from "../../../assets/papir/papyrus13.jpg";
+import image17 from "../../../assets/papir/papyrus14.jpg";
+import image18 from "../../../assets/papir/000010.jpg";
 
 
 import { useRouter } from 'next/navigation';
 
 const page = () => {
+  const [isMobile,setIsMobile]=useState(false)
+  const [width, setWidth]=useState(screen.width)
+  useEffect(()=>{
+    setWidth(screen.width);
+    
+    const windowResize=()=>{
+      setWidth(screen.width);
+    }
+    window.addEventListener("resize",windowResize)
+
+    return()=>{
+      window.addEventListener("resize", windowResize);
+    }
+  },[])
+
+  useEffect(()=>{
+    if (width < 480){
+      setIsMobile(true)
+    }
+    else {setIsMobile(false)
+
+    }
+  },[width])
+
   const images = [
     image1.src,
     image2.src,
@@ -36,42 +60,66 @@ const page = () => {
     image4.src,
     image5.src,
     image6.src,
-    image7.src,
-    image8.src,
-    image9.src,
-    image10.src, ];
-  // const images1 = [
-  //   image11.src,
-  //   image12.src,
-  //   image13.src,
-  //   image14.src,
-  //   image15.src,
-  //   image16.src,
-  //   image17.src,
-  //   image18.src,
-  //   image19.src,
-  //   image20.src,
-  // ];
+];
+  const images1 = [
+    image11.src,
+    image12.src,
+    image13.src,
+    image14.src,
+    image15.src,
+    image16.src,
+  ];
  
   const router=useRouter()
   return (
     <div className={styles.wrapper}>
-      <div className={styles.text}>Текст.</div>
-      <div>Сюда я так полагаю нужны карточки с видами папируса. </div>
-      <div className={styles.image}>
+      <center className={styles.text}>Текст. <br/>Сюда я так полагаю нужны карточки с видами папируса</center>
+      {/* <div className={styles.image}>
         Картинки.
         <div className={styles.images}>
-          {" "}
-          {images.map((src, index) => (
+        {images.map((src, index) => (
             <img key={index} src={src} />
           ))}
         </div>
-        {/* <div className={styles.images1}>
-          {" "}
-          {images.map((src, index) => (
+        <div className={styles.images1}>
+          {images1.map((src, index) => (
             <img key={index} src={src} />
           ))}
-        </div> */}
+        </div>
+      </div> */}
+      <div className={styles1.wrapper}>
+        <HTMLFlipBook
+          width={isMobile ? 330 : 550}
+          height={isMobile ? 450 : 700}
+          className={styles1.book}
+        >
+          <div className={styles1.firstPage}>
+            <div className={styles.images}>
+              {images.map((src, index) => (
+                <img key={index} src={src} />
+              ))}
+            </div>
+          </div>
+          <div className={styles1.secondPage}>
+            <textarea
+              name=""
+              defaultValue="Привет привет привет привет"
+            ></textarea>
+          </div>
+          <div className={styles1.thirdPage}>
+            <textarea name="" defaultValue="Дима, это победа."></textarea>
+          </div>
+          <div className={styles1.secondPage}>
+            <textarea
+              name=""
+              defaultValue="Привет Дима, это победа."
+            ></textarea>
+          </div>
+          <div className={styles1.firstPage}></div>
+          <div className={styles1.secondPage}></div>
+          <div className={styles1.firstPage}>Page 7</div>
+          <div className={styles1.secondPage}>Page 8</div>
+        </HTMLFlipBook>
       </div>
       <button
         onClick={() => router.push("/products")}
