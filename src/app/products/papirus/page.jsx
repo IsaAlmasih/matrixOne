a7d5ebb11343a22b7322b694c2dcf685130ff578
Savva <imperiya66.ru@gmail.com>
2025-01-21 13:51:04 +0300

    Ivan/Savva

--- src/app/products/papirus/page.jsx
+++ src/app/products/papirus/page.jsx
@@ -1,102 +1,82 @@
 "use client"
-import React, { use, useEffect, useState } from 'react'
-import styles from './styles1.module.css'
+import React, { use, useEffect, useRef, useState } from 'react'
+import styles from './styles.module.css'
 import { MatrixRain } from '@/components/MatrixRain/MatrixRain';
 
-import styles1 from "./styles.module.css";
+import { FaArrowAltCircleLeft } from "react-icons/fa";
+import ImageWithModal from '@/components/ImageWithModal/ImageWithModal';
+
+import styles1 from "./styles1.module.css";
 import HTMLFlipBook from "react-pageflip";
 
 import image1 from "../../../assets/papir/papir6.jpg";
 import image2 from "../../../assets/papir/papir8.jpg";
 import image3 from "../../../assets/papir/papir9.jpg";
-import image4 from "../../../assets/papir/papyrus1.jpg";
-import image5 from "../../../assets/papir/papyrus2.jpg";
-import image6 from "../../../assets/papir/papyrus3.jpg";
-import image7 from "../../../assets/papir/papyrus4.jpg";
-import image8 from "../../../assets/papir/papyrus5.jpg";
-import image9 from "../../../assets/papir/papyrus6.jpg";
-import image10 from "../../../assets/papir/papyrus7.jpg";
 import image11 from "../../../assets/papir/papyrus8.jpg";
-import image12 from "../../../assets/papir/papyrus9.jpg";
-import image13 from "../../../assets/papir/papyrus10.jpg";
-import image14 from "../../../assets/papir/papyrus11.jpg";
-import image15 from "../../../assets/papir/papyrus11.jpg";
-import image16 from "../../../assets/papir/papyrus13.jpg";
-import image17 from "../../../assets/papir/papyrus14.jpg";
-import image18 from "../../../assets/papir/000010.jpg";
-
 
 import { useRouter } from 'next/navigation';
 
 const page = () => {
-  const [isMobile,setIsMobile]=useState(false)
-  const [width, setWidth]=useState(1920)
-  useEffect(()=>{
+  const [isMobile, setIsMobile] = useState(false);
+  const [width, setWidth] = useState(1920);
+  useEffect(() => {
     setWidth(screen.width);
-    
-    const windowResize=()=>{
+
+    const windowResize = () => {
       setWidth(screen.width);
-    }
-    window.addEventListener("resize",windowResize)
+    };
+    window.addEventListener("resize", windowResize);
 
-    return()=>{
+    return () => {
       window.addEventListener("resize", windowResize);
-    }
-  },[])
+    };
+  }, []);
 
-  useEffect(()=>{
-    if (width < 480){
-      setIsMobile(true)
+  useEffect(() => {
+    if (width < 480) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
     }
-    else {setIsMobile(false)
+  }, [width]);
 
-    }
-  },[width])
+  const images = [image1.src, image2.src, image3.src];
+  const images1 = [image11.src];
 
-  const images = [
-    image1.src,
-    image2.src,
-    image3.src,
-    image4.src,
-    image5.src,
-    image6.src,
-];
-  const images1 = [
-    image11.src,
-    image12.src,
-    image13.src,
-    image14.src,
-    image15.src,
-    image16.src,
-  ];
- 
-  const router=useRouter()
+  const router = useRouter();
+  const book = useRef();
   return (
     <div className={styles.wrapper}>
-      <center className={styles.text}>Текст. <br/>Сюда я так полагаю нужны карточки с видами папируса</center>
-      {/* <div className={styles.image}>
-        Картинки.
-        <div className={styles.images}>
-        {images.map((src, index) => (
-            <img key={index} src={src} />
-          ))}
-        </div>
-        <div className={styles.images1}>
-          {images1.map((src, index) => (
-            <img key={index} src={src} />
-          ))}
-        </div>
-      </div> */}
       <div className={styles1.wrapper}>
         <HTMLFlipBook
           width={isMobile ? 330 : 550}
           height={isMobile ? 450 : 700}
           className={styles1.book}
+          useMouseEvents={false}
+          ref={book}
         >
-          <div className={styles1.firstPage}>
+          <div className={styles.image}>
+            {/* Картинки. */}
             <div className={styles.images}>
               {images.map((src, index) => (
-                <img key={index} src={src} />
+                <ImageWithModal
+                  alt={"Фотографии папируса"}
+                  width={300}
+                  height={400}
+                  src={src}
+                  key={index}
+                />
+              ))}
+            </div>
+            <div className={styles.images}>
+              {images1.map((src, index) => (
+                <ImageWithModal
+                  alt={"Фотографии папируса"}
+                  width={300}
+                  height={400}
+                  src={src}
+                  key={index}
+                />
               ))}
             </div>
           </div>
@@ -120,6 +100,13 @@ const page = () => {
           <div className={styles1.firstPage}>Page 7</div>
           <div className={styles1.secondPage}>Page 8</div>
         </HTMLFlipBook>
+      </div>
+      <div className={styles.bookButton}>
+        <button onClick={() => book.current.pageFlip().flipPrev()}>
+          {" "}
+          <FaArrowAltCircleLeft />{" "}
+        </button>
+        <button onClick={() => book.current.pageFlip().flipNext()}> +1 </button>
       </div>
       <button
         onClick={() => router.push("/products")}
